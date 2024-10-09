28ecc15f17013af8c1192efc38235867e5275d59
yumis56 <104646391+yumis56@users.noreply.github.com>
2024-10-09 10:04:51 -0400

    Update testRabbitMQClient.php

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -11,16 +11,18 @@ if (isset($argv[1]))
 }
 else
 {
-  $msg = "test message";
+  //$msg = "test message";
+  $msg = "ERROR: No message provided. Please specify a message as an argument."
 }
 
+//TODO eventually, don't hard-code 'steve' and 'password'
 $request = array();
 $request['type'] = "Login";
 $request['username'] = "steve";
 $request['password'] = "password";
 $request['message'] = $msg;
-$response = $client->send_request($request);
-//$response = $client->publish($request);
+$response = $client->send_request($request); //this is for authentication/processing
+//$response = $client->publish($request); //sends same request but different method, maybe broadcasting/logging ??
 
 echo "client received response: ".PHP_EOL;
 print_r($response);
