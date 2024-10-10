53ce0652f50dfc825f1dc518e0b0ba8cf35cbc9f
Mike Gabriel Ayson <maa57@njit.edu>
2024-10-10 13:44:53 -0400

    added echo statements to check to see if the code was successfully retrieved

--- sendRequest.php
+++ sendRequest.php
@@ -1,18 +1,25 @@
 <?php 
 //Got Code from this youtube video https://www.youtube.com/watch?v=bj4GFsv3_Yc&ab_channel=ProgrammingWithSpirit : accessed 10/10/2024 11:07AM// 
 require_once __DIR__ . '/vendor/autoload.php'; 
-use PhpAmqLib/Connection/AMQStreamConnection; 
-use PhpAmqLib/Message/AMQPMessage;
+use PhpAmqpLib/Connection/AMQStreamConnection; 
+use PhpAmqpLib/Message/AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$username = $_POST['username'];
+
+$password = $_POST['password'];
+ 
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest'); //creates new AMQP connection 
 
 $channel->queue_declare('hello', false, false, false, false );
 
 $msg = new AMQPMessage('Hello World'); 
 $channel->basic_publish($msg, '', 'hello');
 
-echo " [x] 'test message' \n";
+echo $username + $password + " [x] 'test message' \n";
 
 $channel->close();
 $connection->close();
+
+//USE FOR LATER: $_POST['username'], $_POST['password'];
 ?> 
