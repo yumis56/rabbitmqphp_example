2f7ca3415f09ac2eb2c806cbf41ac861d7cc20c9
pd438 <pd438@njit.edu>
2024-10-30 20:04:26 -0400

    mikey update

---
+++ rate-review.php
@@ -0,0 +1,10 @@
+<form>
+        <label for="rating" max="5" min="1">Rate the Location (Between 1-5) </label><br>
+        <input type="number" id="fname" name="fname"><br>
+        <label for="lname">Review:</label><br>
+        <textarea id="box" name="review_box" rows="10" cols="50"> 
+            <!--Got code for textarea from IS117 final project which involed using a textarea, needed a bigger box for reviews-->
+            <!--Code edited by Mikey, VSCode was being weird, had someone else do it instead-->
+        </textarea>
+      </form>
+</html>
