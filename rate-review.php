f7d959f78bf3cf6b8b12039d9f9b6c2fb14c036c
yumis56 <104646391+yumis56@users.noreply.github.com>
2024-11-07 00:13:35 -0500

    Update rate-review.php

--- rate-review.php
+++ rate-review.php
@@ -15,28 +15,28 @@
     ?>
 
     <form method="POST" action="rate-review-handler.php" enctype="multipart/form-data">
-        <label for="guestName">Guest Name:</label><br>
+        <label for="guest_name">Guest Name:</label><br>
         <input type="text" id="guestName" name="guestName" required><br><br>
 
         <label for="rating">Rate the Location (Between 1-5):</label><br>
         <input type="number" id="rating" name="rating" min="1" max="5" required><br><br>
 
-        <label for="review">Review:</label><br>
+        <label for="review_text">Review:</label><br>
         <textarea id="review" name="review" rows="10" cols="50" required></textarea><br><br>
         
         <label for="location">Location:</label><br>
         <input type="text" id="location" name="location" required><br><br>
 
-        <label for="date">Date of Visit:</label><br>
+        <label for="visit_date">Date of Visit:</label><br>
         <input type="date" id="date" name="date" required><br><br>
 
         <label for="photo">Upload Photo (optional):</label><br>
         <input type="file" name="photo" id="photo" accept="image/*"><br><br>
-
+<br><br>
         <button type="submit">Submit Review</button>
     </form>
 
-    <br>
+    <br><br>
     <a href="book.php" class="button">Book a Flight</a>
     <a href="/Apache/html/logout.php">Logout</a>
 </body>
